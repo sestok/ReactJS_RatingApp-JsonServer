4fcbe0d90aeb2cfc5ae7c0b4bd556f8978322868
Sami Alhamad <sakosalo@outlook.com>
2022-04-16 22:51:58 +0300

    Phase1

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,16 @@
+import Card from './shared/Card'
+import { FaTimes } from 'react-icons/fa'
+
+function FeedbackItem({item}) {
+  return (
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button className="close">
+            <FaTimes color='purple'/>
+        </button>
+        <div className="text-display">{item.text}</div>
+    </Card>
+  )
+}
+
+export default FeedbackItem

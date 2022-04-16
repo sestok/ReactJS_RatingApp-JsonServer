2ccdb20f99ef9aa65bcddd7f1095f7251ffac44b
Sami Alhamad <sakosalo@outlook.com>
2022-04-17 00:51:26 +0300

    Importing PropTypes

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function FeedbackStats({feedback}) {
 
     // Calculate Feedbacks Average
@@ -6,13 +8,17 @@ function FeedbackStats({feedback}) {
     }, 0) / feedback.length
 
     average = average.toFixed(1).replace(/[.,]0$/, '')
-    
+
   return (
     <div className='feedback-stats'>
         <h4>{feedback.length} Feedbacks</h4>
         <h4>Average Feedbacks: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats

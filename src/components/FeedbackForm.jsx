5a7a16e4175adbb5787623614624c1007000e27f
Sami Alhamad <sakosalo@outlook.com>
2022-04-19 01:58:07 +0300

    Passing editfeedback to the context

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -13,7 +13,11 @@ function FeedbackForm() {
   const { addFeedback, feedbackEdit } = useContext(FeedbackContext)
 
   useEffect(() =>{
-
+    if(feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
   }, [feedbackEdit])
 
   const handleTextChange = (e) => {

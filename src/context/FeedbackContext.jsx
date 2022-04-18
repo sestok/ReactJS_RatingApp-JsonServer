b7d1675320c3a0e6add7503d9c33cdd716365b20
Sami Alhamad <sakosalo@outlook.com>
2022-04-18 23:30:07 +0300

    Assigning all functions to Context

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from 'react'
 
 const FeedbackContext = createContext()
@@ -6,15 +7,28 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'Checking if Provider works',
+      text: 'Checking is coming from the Context',
       rating: 5,
     },
   ])
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Wanna delete it?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
 
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
       }}
     >
       {children}

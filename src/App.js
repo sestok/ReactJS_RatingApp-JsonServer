82ea95d5b9b747c8d69533b5dbb24e2e13aae89e
Sami Alhamad <sakosalo@outlook.com>
2022-04-18 23:30:52 +0300

    Assigning all functions to Context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -11,19 +9,6 @@ import AboutLink from './components/AboutLink'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Wanna delete it?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   return (
     <FeedbackProvider>
       <Router>
@@ -35,12 +20,9 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackStats feedback={feedback} />
-                  <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                  />
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
                 </>
               }
             ></Route>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 import FeedbackItem from './FeedbackItem'
 import { motion, AnimatePresence } from 'framer-motion'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedbacks yet!</p>
   }
@@ -16,11 +20,7 @@ function FeedbackList({ feedback, handleDelete }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={handleDelete}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
